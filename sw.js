d611024402fd7290c46038ba839fd43486f14761
vdammario-wq <vdam.mario@gmail.com>
2026-05-29 11:35:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,36 +1,35 @@
-const CACHE = 'onescrives-v2';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE = 'scrives-v6';
+const STATISCH = [
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE).then(c => c.addAll(STATISCH))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    )
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  if (e.request.mode === 'navigate') {
-    // Network-first voor navigatie (index.html altijd vers)
+  const url = new URL(e.request.url);
+  if(url.pathname.endsWith('/') || url.pathname.endsWith('index.html')){
     e.respondWith(
-      fetch(e.request)
-        .then(res => {
-          const clone = res.clone();
-          caches.open(CACHE).then(c => c.put(e.request, clone));
-          return res;
-        })
-        .catch(() => caches.match(e.request))
+      fetch(e.request).catch(() => caches.match('./index.html'))
     );
-  } else {
-    // Cache-first voor overige assets
-    e.respondWith(
-      caches.match(e.request).then(r => r || fetch(e.request))
-    );
+    return;
   }
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
 });
